ad88217bfdbfd6d3b1ba99a31294bf6e1887e0d8
yipfram <46826777+yipfram@users.noreply.github.com>
2023-10-04 15:42:03 +0200

    Week 5 commit

--- week5/Rscript.r
+++ week5/Rscript.r
@@ -1,1 +1,11 @@
+data(mtcars)
+# install.packages("tidyverse")
 library(tidyverse)
+library(dplyr)
+
+# Filter cars that have disp bigger than 200 and put it in mtcars_tr variable
+mtcars_tr <- mtcars |> filter(disp > 200) |> # nolint
+                    select(mpg, disp) |> # nolint: indentation_linter.
+                    mutate(mpg_disp = mpg * disp)
+
+view(mtcars_tr)
